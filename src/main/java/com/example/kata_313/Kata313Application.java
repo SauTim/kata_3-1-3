db4494df8408d07303fe8309fa7954868d2cc6f2
Timur <tim1999997@gmail.com>
2023-11-11 12:00:33 +0500

    Base project

--- src/main/java/com/example/kata_313/Kata313Application.java
+++ src/main/java/com/example/kata_313/Kata313Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Kata313Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Kata313Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Kata313Application.class, args);
+    }
 
 }
